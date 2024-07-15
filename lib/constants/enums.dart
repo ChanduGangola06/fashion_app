7b13393ac01efb0c064c176d27d1849e4bfe10f7
Chandu Gangola <chandugangola444@gmail.com>
2024-07-15 23:03:40 +0530

    working

---
+++ lib/constants/enums.dart
@@ -0,0 +1,24 @@
+enum QueryType { all, popular, unisex, men, women, kids }
+
+enum WishlistToggle {
+  retrieve,
+  delete,
+  add,
+}
+
+enum FetchOrdersTypes {
+  pending,
+  delivered,
+  cancelled,
+}
+
+enum ReloadCount {
+  delete,
+  reload,
+  update,
+}
+
+enum NotificationCount {
+  update,
+  reload,
+}
